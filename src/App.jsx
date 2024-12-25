2a0a03b70ffbde602be5b1bef98a06e862c05d92
iMythms <maythams.149@gmail.com>
2024-12-25 20:04:37 +0300

    implement user authentication; add sign up and sign in services, update profile retrieval, and enhance routing in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,52 @@ import Home from './pages/Home'
 import Signin from './pages/auth/Signin'
 import Signup from './pages/auth/Signup'
 import Dashboard from './pages/Dashboard'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { getProfile } from './services/userService'
 
 function App() {
-  const [user, setUser] = useState({})
-  return (
-    <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/dashboard' element={<Dashboard user={user} />} />
-          <Route path='/auth/signup' element={<Signup />} />
-          <Route path='/auth/signin' element={<Signin />} />
-        </Routes>
-      </main>
-    </>
-  )
+	const [user, setUser] = useState(null)
+
+	const getUserProfile = async () => {
+		try {
+			const data = await getProfile()
+			setUser(data)
+		} catch (error) {
+			setUser(null)
+			console.log(error)
+		}
+	}
+
+	const logOut = () => {
+		localStorage.removeItem('authToken')
+		setUser(null)
+	}
+
+	useEffect(() => {
+		getUserProfile()
+	}, [])
+
+	return (
+		<>
+			<header>
+				<NavBar />
+			</header>
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/dashboard" element={<Dashboard user={user} />} />
+					<Route
+						path="/auth/signup"
+						element={<Signup getUserProfile={getUserProfile} />}
+					/>
+					<Route
+						path="/auth/signin"
+						element={<Signin getUserProfile={getUserProfile} />}
+					/>
+				</Routes>
+			</main>
+		</>
+	)
 }
 
 export default App
--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -1,61 +1,70 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { signIn } from '../../services/authService'
 
 const initialFormData = {
-    username: '',
-    password: '',
+	username: '',
+	password: '',
 }
-const Signin = () => {
-    const [message, setMessage] = useState('');
-    const [formData, setFormData] = useState(initialFormData);
+const Signin = ({ getUserProfile }) => {
+	const [message, setMessage] = useState('')
+	const [formData, setFormData] = useState(initialFormData)
+	const navigate = useNavigate()
 
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value })
+	}
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			await signIn(formData)
+			await getUserProfile()
+			setFormData(initialFormData)
+			navigate('/dashboard')
+		} catch (error) {
+			setMessage(error.response?.data?.error)
+			console.log(error)
+		}
+		console.log(formData)
+	}
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(formData);
+	return (
+		<main>
+			<h1>Log In</h1>
+			<p style={{ color: 'red' }}>{message}</p>
+			<form autoComplete="off" onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="email">Username:</label>
+					<input
+						type="text"
+						autoComplete="off"
+						id="username"
+						value={formData.username}
+						name="username"
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						autoComplete="off"
+						id="password"
+						value={formData.password}
+						name="password"
+						onChange={handleChange}
+					/>
+				</div>
+				<section>
+					<button>Log In</button>
+					<Link to="/">
+						<button>Cancel</button>
+					</Link>
+				</section>
+			</form>
+		</main>
+	)
+}
 
-    };
-
-    return (
-        <main>
-            <h1>Log In</h1>
-            <p style={{ color: 'red' }}>{message}</p>
-            <form autoComplete="off" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Username:</label>
-                    <input
-                        type="text"
-                        autoComplete="off"
-                        id="username"
-                        value={formData.username}
-                        name="username"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        autoComplete="off"
-                        id="password"
-                        value={formData.password}
-                        name="password"
-                        onChange={handleChange}
-                    />
-                </div>
-                <section>
-                    <button>Log In</button>
-                    <Link to="/">
-                        <button>Cancel</button>
-                    </Link>
-                </section>
-            </form>
-        </main>
-    );
-};
-
-export default Signin;
+export default Signin
